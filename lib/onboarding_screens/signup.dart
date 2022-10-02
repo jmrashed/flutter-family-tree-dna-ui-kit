a63ade1e42520dfe5b78acdf79885873d6620a4c
Md Rasheduzzaman <jmrashed@users.noreply.github.com>
2022-10-03 01:59:34 +0600

    upATED

--- lib/onboarding_screens/signup.dart
+++ lib/onboarding_screens/signup.dart
@@ -12,8 +12,38 @@ class SignIn extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Center(child: loadLogo(image: "assets/images/logo.png")),
-        Center(child: Text('Sign In',   style: GoogleFonts.lora(fontSize: 32, fontWeight: FontWeight.bold),)),
+        Center(child: Text('Sign Up',   style: GoogleFonts.lora(fontSize: 32, fontWeight: FontWeight.bold),)),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text('Write your name'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+              labelText: 'Enter your username',
+            ),
+          ),
+        ),
 
+         Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text('Write your Password'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+              labelText: 'Enter your Password',
+              suffixIcon: Icon(Icons.visibility)
+            ),
+          ),
+        ), 
+        
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text('Write your Email'),
@@ -28,38 +58,20 @@ class SignIn extends StatelessWidget {
             ),
           ),
         ),
-
-
-
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextFormField(
-            decoration: InputDecoration(
-                border:
-                OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
-                labelText: 'Password',
-                suffixIcon: Icon(Icons.visibility)
-            ),
-          ),
-        ),
-
-
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: SizedBox(
             width: double.infinity,
-            height: 50,
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                 shape: StadiumBorder(),
-                backgroundColor: Colors.green,
-
+                backgroundColor: Colors.green
               ),
-
-
+              
+              
               onPressed: () {
-
-            }, child: Text("Sign In") ),
+              
+            }, child: Text("Registration") ),
           ),
         ),
         Center(child: Text("-----OR------")),
